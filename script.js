7615710d613ef9bb850c636825c901e79b26202d
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-05 20:59:02 -0400

    removing text

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function displayWeatherCondition(response) {
   let iconElement = document.querySelector("icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/04d@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
