f0ba1cc390aef4fc9561c5237e870c584fcfb9d1
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-07 00:05:48 -0400

    added description

--- script.js
+++ script.js
@@ -26,10 +26,11 @@ function formatDate(date) {
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let descriptionElement = (temperatureElement.innerHTML = Math.round(
-    response.data.main.temp
-  ));
+  let descriptionElement = document.querySelector("#descripton");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 let apiKey = "39cdec0e8624a940458fa04e89274d6c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
