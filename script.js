e3d030f87443c309fbb4b6f30a4b3c80ba8709e4
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-07 00:26:24 -0400

    updated wind element

--- script.js
+++ script.js
@@ -27,10 +27,12 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#descripton");
+  let windElement = document.querySelector("#wind");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "39cdec0e8624a940458fa04e89274d6c";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
