c045a56d93e377aded1a563ac1331100f2983846
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-08 12:43:33 -0400

    Working on weather icon

--- script.js
+++ script.js
@@ -28,12 +28,13 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-}
+  iconElement.innerHTML = `http://openweathermap.org/img/wn/04d@2x.png`;
 
 function search(city) {
   let apiKey = "39cdec0e8624a940458fa04e89274d6c";
