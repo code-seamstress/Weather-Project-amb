a3e2356cedb3d06237b9fe0e1c57ba0cd7dccec0
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-23 12:34:07 -0400

    Adding future forecast code

--- script.js
+++ script.js
@@ -31,7 +31,8 @@ function formatDay(timestamp) {
   return days[day];
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -65,6 +66,8 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
     lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+
 }
 
 
@@ -132,6 +135,6 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
+
 
 
