a508ab77be4d813a780614b54d5acdfcdb3cd395
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-23 12:37:34 -0400

    Console logging

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
