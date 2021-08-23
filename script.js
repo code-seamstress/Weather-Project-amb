4853baaa52b938db6b141bdba42dd688047c3055
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-23 12:19:05 -0400

    console logging

--- script.js
+++ script.js
@@ -61,8 +61,10 @@ ${day}
 
 function getForecast(coordinates) {
   console.log(coordinates);
-
-
+  let apiKey = "39cdec0e8624a940458fa04e89274d6c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
+    lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
 }
 
 
