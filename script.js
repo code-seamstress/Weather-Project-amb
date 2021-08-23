feb2d9fb7e1aa149dc19ba4d38bba3a79774d5cf
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-23 12:46:00 -0400

    Made link secure

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
