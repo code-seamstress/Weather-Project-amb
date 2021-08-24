3139119853d846388e65d4d0cfe86125b93c1ac4
code-seamstress <83052977+code-seamstress@users.noreply.github.com>
2021-08-23 23:31:54 -0400

    Worked on adding days of week in

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+
+
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
@@ -40,6 +42,8 @@ function displayForecast(response) {
     forecastHTML = forecastHTML + `
    
 <div class="col-2">
+<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+
 ${forecastDay.temp.max}°
     <br />
    <img
